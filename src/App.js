7e79e6a59c2a8ea72f58cdcf427399b9eba238c8
github-usman <usmantoyou@gmail.com>
2024-02-01 12:30:22 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SummaryScreen from './pages/SummaryScreen';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -7,6 +8,7 @@ function App() {
 	return (
 		<div>
 			<BrowserRouter>
+				<Navbar/>
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/summary/:show-detail' element={<SummaryScreen />} />
--- src/pages/SummaryScreen.js
+++ src/pages/SummaryScreen.js
@@ -8,6 +8,7 @@ const SummaryScreen = () => {
 		<div>
 			{apiData && (
 				<>
+					{/* image design and access or uppper side */}
 					<div className='welcome-poster'>
 						<div className='summary-images'>
 							<div className='summary-images-inner'>
@@ -30,23 +31,25 @@ const SummaryScreen = () => {
 							</div>
 							<div>
 								<div className='image-btn-box'>
-									<h1>{apiData.show.name}</h1>
+									<h2>{apiData.show.name}</h2>
 									<div className='image-btn-child  mt-3'>
 										<img src={star} alt='star' />
-										<h3 className='mt-1'>
+										<h4 className='mt-1'>
 											{apiData.show.rating.average !== null ? apiData.show.rating.average : 5.5}
 											/10
-										</h3>
-										<p className='mt-2'>{(apiData.show.externals.thetvdb / 1000).toFixed(1)}k Views</p>
+										</h4>
+										<p className='mt-3'>{(apiData.show.externals.thetvdb / 1000).toFixed(1)}k Views</p>
 									</div>
-									
-									<p style={{backgroundColor:"white",color:"black",width:'fit-content',padding:'0.1rem'}}>{apiData.show.language}</p>
+									<div className='d-flex'>
+									<p style={{backgroundColor:"white",color:"black",width:'fit-content',padding:'0.1rem',borderRadius:'4px',display:"block"}}>{apiData.show.language}</p>
+									<p style={{backgroundColor:"white",color:"black",width:'fit-content',padding:'0.1rem',borderRadius:'4px',display:"block"}} className='mx-2'>{apiData.show.network.country.name}</p>
+									</div>
 									<div className='image-box-details'>
-									<p>{hours}h {minutes}m</p>
+									<p>{hours}h {minutes}m &nbsp;&nbsp;<b>.</b></p>
 									{apiData.show.genres.map(element => (
-										<p>{element}</p>
+										<p>{element},</p>
 									))}
-									
+									<b>.</b>
 									<p>{new Date(apiData.show.ended).toDateString()}</p>
 									</div>
 									<button className='btn btn-danger btn-lg mt-3'>Book Show</button>
@@ -54,16 +57,19 @@ const SummaryScreen = () => {
 							</div>
 						</div>
 					</div>
-					<div className='container'>
-						<p>{apiData.score}</p>
-						{/* <p>{apiData.show.id}</p> */}
-						<p>{apiData.show.url}</p>
-						<p>{apiData.show.name}</p>
-						<p>{apiData.show.type} = type</p>
-						
-						
-						<p>{apiData.show.status}</p>
 
+					{/* bottom side of summary page */}
+					<div className='container mt-3'>
+						<h4 className=' d-inline'>About the Show </h4> <span className={`badge bg-${
+							apiData.show.status === 'Running' ? 'success' : 'danger'
+						}`} >{apiData.show.status} </span>
+						<p dangerouslySetInnerHTML={{__html:apiData.show.summary}}></p>
+						<a href={apiData.show.url} className='btn btn-outline-success me-5'>Know more...</a>
+						<a href={apiData.show.officialSite} className='btn btn-outline-warning '>Got to Official Website</a>
+						<hr className='my-5'/>
+					    {/* <p>show premiered date {apiData.show.premiered}</p>
+					    <p>show Ended date {apiData.show.ended}</p>
+						<p>{apiData.show.type} = type</p>
 						
 						<p>{apiData.show.averageRuntime}</p>
 						<p>{apiData.show.premiered}</p>
@@ -71,22 +77,11 @@ const SummaryScreen = () => {
 						<p>{apiData.show.officialSite}</p>
 						
 						<p>{apiData.show.schedule.days}</p>
-						<p>{apiData.show.rating.average}</p>
-						<p>{apiData.show.rating.average}</p>
 						<p>{apiData.show.weight}</p>
-						{/* <p>{apiData.show.network.id}</p> */}
-						{/* <p>{apiData.show.network.name}</p> */}
-						{/* <p>{apiData.show.network.country.name}</p> */}
-						{/* <p>{apiData.show.network.country.code}</p> */}
-						{/* <p>{apiData.show.network.country.timezone}</p> */}
-						{/* <p>{apiData.show.network.officialSite}</p> */}
-						{/* <p>{apiData.show.network.webChannel}</p> */}
-						{/* <p>{apiData.show.network.dvdCountry}</p> */}
 						<p>{apiData.show.externals.tvrage}</p>
 						<p>{apiData.show.externals.thetvdb}</p>
 						<p>{apiData.show.externals.imdb}</p>
-						<p>{apiData.show.summary}</p>
-						<p>{apiData.show.updated}</p>
+					 */}
 						{/* <p>{apiData.show._links.self.href}</p> */}
 						{/* <p>{apiData.show._links.previousepisode.href}</p> */}
 					</div>
